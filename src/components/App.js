e4df6ca717c4b303f513273465d6fb77d54c8b1e
Michal Siminski <michal.siminski3@gmail.com>
2021-03-04 10:05:29 +0100

    starting structure

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+
+import '../css/reset.css';
+
+function App() {
+  return (
+      <>
+        <Header/>
+        <Navigation/>
+        <Page/>
+        <Footer/>
+      </>
+  );
+}
+
+export default App;
